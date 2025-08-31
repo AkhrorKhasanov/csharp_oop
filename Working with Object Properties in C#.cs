c08aa0b588e8de5f7613ee2dd54302db9e06f61c
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2025-08-31 11:13:28 +0500

    31.08.2025 11:13

--- Working with Object Properties in C#.cs
+++ Working with Object Properties in C#.cs
@@ -4,7 +4,7 @@ class Employee
 {
     public string Name { get; set; }
     public int Age { get; set; }
-    public decimal Salary { get; set; }
+    public int Salary { get; set; }
 }
 
 class Program
@@ -14,10 +14,15 @@ class Program
         Employee emp = new Employee();
         emp.Name = "John";
         emp.Age = 25;
-        emp.Salary = 1000.25m;
+        emp.Salary = 1000;
 
-        Console.WriteLine($"Name: {emp.Name}");
-        Console.WriteLine($"Age: {emp.Age}");
-        Console.WriteLine($"Salary: {emp.Salary}");
+        Employee emp2 = new Employee();
+        emp2.Name = "Eric";
+        emp2.Age = 26;
+        emp2.Salary = 2000;
+
+        Console.WriteLine($"Sum of the salaries: {emp.Salary + emp2.Salary}");
+        Console.WriteLine($"Sum of the ages: {emp.Age + emp2.Age}");
+
     }
 }
